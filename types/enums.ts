f096e4ca6fd52c364b9aba94300766bd845d7048
kapepa <kapepayx@gmail.com>
2024-08-06 11:17:18 +0300

    home page, nav, kinde-auth-nextjs

---
+++ types/enums.ts
@@ -0,0 +1,8 @@
+export enum RoutesPaths {
+  Home = "/",
+  Dashboard = "/dashboard",
+  ConfigureUpload = "/configure/upload",
+  ApiLoin = "/api/auth/login",
+  ApiLogout = "/api/auth/logout",
+  ApiRegister = "/api/auth/register",
+}
