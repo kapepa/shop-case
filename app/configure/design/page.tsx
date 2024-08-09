407cebf68fed083b1a8eb158ea4ecdccd955fdbb
kapepa <kapepayx@gmail.com>
2024-08-09 08:32:57 +0300

    feature/configuration

--- app/configure/design/page.tsx
+++ app/configure/design/page.tsx
@@ -1,10 +1,33 @@
+import prisma from "@/db";
 import { NextPage } from "next";
+import { notFound } from "next/navigation";
+import { DisignConfiguration } from "./components/disign-configuration";
 
-const DesignPage: NextPage = async () => {
+interface DesignPageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined,
+  }
+}
+
+const DesignPage: NextPage<DesignPageProps> = async ({ searchParams }) => {
+  const { id } = searchParams;
+  if (!id || typeof id !== "string") return notFound();
+
+  const configuration = await prisma.configuration.findUnique({
+    where: {
+      id
+    }
+  })
+  if (!configuration) return notFound();
+
+  const { imageUrl, width, height } = configuration;
+
   return (
-    <div>
-      DesignPage
-    </div>
+    <DisignConfiguration
+      imageUrl={imageUrl}
+      configId={id}
+      imageDimensions={{ height, width }}
+    />
   )
 }
 
