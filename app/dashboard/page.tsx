456a9268b81ff8e290989d94d1d6ed5c60e56fdd
kapepa <kapepayx@gmail.com>
2024-08-20 17:41:24 +0300

    payment

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+const DashboardPage: NextPage = () => {
+  return (
+    <div
+      className=" flex min-h-screen w-full bg-muted/40"
+    >
+
+    </div>
+  )
+}
+
+export default DashboardPage;
