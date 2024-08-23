5caae677030b4b4d917c6ab5d9c34f31daad1307
kapepa <kapepayx@gmail.com>
2024-08-23 18:15:23 +0300

    payment

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -3,6 +3,10 @@ import { stripe } from "@/lib/stripe";
 import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
+import { Resend } from 'resend';
+import { OrderReceivedEmail } from "@/components/emails/order-received-email";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST (req: NextRequest) {
   try {
@@ -28,7 +32,7 @@ export async function POST (req: NextRequest) {
       const billingAddress = session.customer_details!.address;
       const shippingAddress = session.shipping_details?.address;
 
-      await prisma.order.update({
+      const updatedOrder = await prisma.order.update({
         where: {
           id: orderId,
         },
@@ -56,9 +60,28 @@ export async function POST (req: NextRequest) {
           }
         }
       })
-
-      return NextResponse.json({ result: event, ok: true })
+      
+      await resend.emails.send({
+        from: "CaseCobra <hellow@joshtriedcoding.com>",
+        to: [event.data.object.customer_details.email],
+        subject: "Thanks for your order!",
+        react: OrderReceivedEmail({ 
+          orderId, orderDate: 
+          updatedOrder.updatedAt.toLocaleDateString(), 
+          // @ts-ignore
+          hippingAddress: {
+            name: session.customer_details!.name!,
+            city: shippingAddress!.city!,
+            country: shippingAddress!.country!,
+            postalCode: shippingAddress!.postal_code!,
+            street: shippingAddress!.line1!,
+            state: shippingAddress!.state!,
+          }
+        })
+      })
     }
+
+    return NextResponse.json({ result: event, ok: true })
   } catch (err) {
     return NextResponse.json({ message: "Something went wrong", ok: false }, { status: 500 });
   }
